0ea0ce158e4c34641f9dc56980fabe036f24c3c8
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2025-12-06 19:44:00 +0200

    login templates and views

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
+from . import views
 from core.views import wordle_view
 
 urlpatterns = [
-   path('', wordle_view, name='wordle'),
+    path('', wordle_view, name='wordle'),
+    path('', views.login_view, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
