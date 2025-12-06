bfac66bdf57dffd27e0a63c989ee21f19dc6071f
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2025-12-06 20:19:33 +0200

    update login & register

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,11 @@ from . import views
 from core.views import wordle_view
 
 urlpatterns = [
-    path('', wordle_view, name='wordle'),
+
     path('', views.login_view, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+        path('game', wordle_view, name='game'),
     # path('dashboard/', views.dashboard, name='dashboard'),
 ]
