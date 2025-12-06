29789fe60ac99e4ffe5f090ba83ad31405b36f99
Faris Rami <105630035+Fizox@users.noreply.github.com>
2025-12-06 19:41:56 +0200

    fixes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-# myapp/views.py
 from django.shortcuts import render
 from django.utils.timezone import now
 from django.conf import settings
@@ -6,7 +5,8 @@ from django.conf import settings
 WORDS = ["APPLE","GRACE","CRANE","PLANT","ABOUT","RIVER","SMILE","BREAD","MOUSE","BRICK","AMONG","TULIP"]
 
 def wordle_view(request):
-    # خيار: اختر كلمة يومية ثابتة بحسب تاريخ (مثال: index من أيام منذ epoch)
     idx = (now().date().toordinal()) % len(WORDS)
-    server_word = WORDS[idx]  # كلمة اليوم (تُمرر للقالب)
+    server_word = WORDS[idx]
     return render(request, "wordle.html", {"server_word": server_word})
+
+
